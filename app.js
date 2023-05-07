839df7a836593431a89aa3fb1e17c3c2180a09c5
osonoi <yosonoi@gmail.com>
2023-05-07 11:12:26 +0900

    add notes.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes'); 
 
 var app = express();
 
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter); 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
