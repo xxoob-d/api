708071e849224ae47dd105cf68970b6e59860c60
osonoi <yosonoi@gmail.com>
2023-05-07 13:26:18 +0900

    add cat.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
-var notesRouter = require('./routes/notes'); 
+var notesRouter = require('./routes/notes');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -16,7 +17,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use('/hello', helloRouter);
-app.use('/notes', notesRouter); 
+app.use('/notes', notesRouter);
+app.use('/cat', catRouter);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
