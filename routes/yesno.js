cd8110b743bfa0cca6f54f52271b25a2080ce70b
xxoob-d <143997258+xxoob-d@users.noreply.github.com>
2023-11-28 17:15:07 +0900

    Update yesno.js

--- routes/yesno.js
+++ routes/yesno.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', async (req, res) => {
-request('https://yesno.wtf/assets/yes/10-271c872c91cd72c1e38e72d2f8eda676.gif', function (error, response, body) {
+request('https://yesno.wtf/api', function (error, response, body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 res.json(data);
